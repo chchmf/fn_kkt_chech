51feaf73a182ac01752ce7a4a60cd2f8155e46ec
chchmf <icheese20@gmail.com>
2022-06-18 01:11:43 +0500

    master

--- kkt async.py
+++ kkt async.py
@@ -27,7 +27,9 @@ async def get_data(session, url, sn):
 		resp_text = await resp.text()
 		print(sn+";"+str(json.loads(resp_text)))
 		json_text = json.loads(resp_text)
-		if json_text["check_status"] == 0:
+		if json_text["error"] == "Произошла техническая ошибка":
+			csv.write(sn+";"+str(json_text["error"])+"\n")
+		elif json_text["check_status"] == 0:
 			csv.write(sn+";"+str(json_text["check_result"])+"\n")
 		else:
 			csv.write(sn+";"+str("Готова к преключениям")+"\n")
